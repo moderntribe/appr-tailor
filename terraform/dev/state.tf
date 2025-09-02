ac3705882cd2b753e2b93b4d7ff4a0a42ab05568
Arthur Guerra <arthur@tri.be>
2025-09-02 08:45:05 +0200

    feat: change bucket tfstate location

--- terraform/dev/state.tf
+++ terraform/dev/state.tf
@@ -6,7 +6,7 @@ terraform {
   backend "s3" {
     encrypt = true
     bucket  = "appr-terraform-state"
-    key     = "appr/dev/app/appr-terraform-state-dev.tfstate"
+    key     = "appr/dev/app/tailor-dev.tfstate"
     region  = "us-east-1"
   }
 }
